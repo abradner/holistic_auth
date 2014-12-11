2dac247b4f1c28935634c19a5ffce6e3691e3999
Alex Bradner <alex@bradner.net>
2014-12-12 01:28:52 +1100

    some tests and more scaffolding

--- lib/authinator/auth_code_exchanger.rb
+++ lib/authinator/auth_code_exchanger.rb
@@ -1,14 +1,35 @@
-
+# module Authinator
 class AuthCodeExchanger
-  def self.exhchange(_provider, _auth_code)
+  def self.exchange(provider, auth_code)
     # auth_code = params[:code]
+    return if auth_code.nil? || auth_code.empty?
+
+    case provider.to_sym
+      when :google
+        exchange_with_google(auth_code)
+      when :stub
+        exchange_with_stub(auth_code)
+    end
   end
 
   private
 
-  def exchange_with_googles(_code)
+  def self.exchange_with_google(_code)
+    {
+        access_token: 'abc',
+        refresh_token: 'def',
+        expires: true,
+        expiry: 3600
+    }
   end
 
-  def exchange_with_stub(_code)
+  def self.exchange_with_stub(_code)
+    {
+        access_token: 'abc',
+        refresh_token: 'def',
+        expires: true,
+        expiry: 3600
+    }
   end
 end
+# end

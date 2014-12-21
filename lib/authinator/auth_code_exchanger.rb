8eb90b982922af984f9e8dd17fa7324e3ecc78f7
Alexander Bradner <alex@bradner.net>
2014-12-22 02:18:14 +1100

    Implemented auth_code->token exchange. needs refactor

--- lib/authinator/auth_code_exchanger.rb
+++ lib/authinator/auth_code_exchanger.rb
@@ -1,38 +1,91 @@
-require 'omniauth/strategies/google_oauth2'
+# require 'omniauth/strategies/google_oauth2'
+# require 'omniauth-oauth2'
+require 'oauth2'
 
 # module Authinator
 class AuthCodeExchanger
-  def self.exchange(provider, auth_code)
+  VALID_PROVIDERS = %i(stub google)
+
+  def self.valid_providers
+    VALID_PROVIDERS
+  end
+  attr_reader :provider
+
+  @env_hash = {
+    provider_ignores_state: true,
+    code: '4/def',
+    callback_path: 'https://callback_path',
+    client_options: { site: 'https://api.somesite.com' },
+    site: 'https://accounts.google.com',
+    token_url: '/o/oauth2/token'
+  }
+
+  PROVIDER_HASHES = {
+    google: {
+      client_id: 'cl_id',
+      client_secret: 'cl_sec',
+      site: 'https://accounts.google.com',
+      token_url: '/o/oauth2/token'
+    },
+    stub: {
+      client_id: 'cl_id',
+      client_secret: 'cl_sec',
+      site: 'https://example.org',
+      token_url: '/extoken'
+    }
+  }
+
+  def initialize(provider)
+    @provider = provider
+    @provider_hash = PROVIDER_HASHES[provider]
+  end
+
+  def exchange(auth_code)
     # auth_code = params[:code]
     return if auth_code.nil? || auth_code.empty?
 
-    case provider.to_sym
-      when :google
-        exchange_with_google(auth_code)
-      when :stub
-        exchange_with_stub(auth_code)
+    case @provider.to_sym
+    when :google
+      exchange_with_google(auth_code)
+    when :stub
+      exchange_with_stub(auth_code)
     end
+  end
+
+  def site_token_url
+    @provider_hash[:site] + @provider_hash[:token_url]
   end
 
   private
 
-  def self.exchange_with_google(_code)
+  def exchange_with_google(code)
+    provider_hash = @provider_hash.dup
 
-    {
-        access_token: 'abc',
-        refresh_token: 'def',
-        expires: true,
-        expiry: 3600
-    }
+    client_id = provider_hash.delete(:client_id)
+    client_secret = provider_hash.delete(:client_secret)
+    @client = OAuth2::Client.new(client_id, client_secret, provider_hash)
+
+    token = @client.auth_code.get_token(code)
+
+    # response = token.get('/api/resource', :params => { 'query_foo' => 'bar' })
+    # response.class.name
+    # => OAuth2::Response
+
+    token
   end
 
-  def self.exchange_with_stub(_code)
-    {
-        access_token: 'abc',
-        refresh_token: 'def',
-        expires: true,
-        expiry: 3600
-    }
+  def exchange_with_stub(_code)
+    @client = OAuth2::Client.new(
+        @provider_hash[:client_id],
+        @provider_hash[:client_secret]
+    )
+
+    OAuth2::AccessToken.new(
+        @client,
+        'ya29.token',
+        refresh_token: '1/refresh',
+        expires_in: 3600
+    )
   end
 end
 # end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,31 @@
 require 'bundler/setup'
 Bundler.setup
 
+if RUBY_VERSION >= '1.9'
+  require 'simplecov'
+  require 'coveralls'
+
+  SimpleCov.formatters = [
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
+
+  SimpleCov.start do
+    minimum_coverage(76)
+  end
+end
+
+require 'rspec'
+require 'rack/test'
+require 'webmock/rspec'
 require 'authinator' # and any other gems you need
 
-RSpec.configure do |_config|
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+  config.include Rack::Test::Methods
+  config.include WebMock::API
+
   # some (optional) config here
 end
